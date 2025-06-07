46a768ae411bf0fe103dfd7cd2cf8fa9f35cbfaa
SASLucas <lsas3@aluno.ifal.edu.br>
2025-06-07 15:39:13 -0300

    Delete routes directory

--- routes/admRout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const funcoesAdm = require('../controller/admController')
-const adicionarDisciplina = new funcoesAdm
-
-router.post('/disciplinas', (res, req) => adicionarDisciplina.cadastrarDisc(res, req))
-
-module.exports = router
